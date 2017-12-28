78d044efa25eec88c2f07da70acc8434dfa68410
Andrew Levy <andrewglevy@gmail.com>
2017-12-27 17:27:23 -0800

    added username to user model; user parameters to application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :collections
+  validates :username, presence: true
+  validates :username, length: { maximum: 16,
+    too_long: "username must be between 4 and 16 characters" }
+  validates :username, length: { minimum: 4,
+    too_short: "username must be between 4 and 16 characters" }
+  validates :username, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
