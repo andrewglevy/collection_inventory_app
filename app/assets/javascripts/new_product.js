f4d9563901824e324888ab9672b9a35c87b42654
Andrew Levy <andrewglevy@gmail.com>
2017-12-29 15:59:49 -0800

    because fiona said so

--- app/assets/javascripts/new_product.js
+++ app/assets/javascripts/new_product.js
@@ -1,12 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $(".new_product_button").click(function(){
-    if ($(this).hasClass("hidden_field")){
+    if ($(".new_product_field").hasClass("new_product_hidden")){
       $(".new_product_hidden").removeClass('new_product_hidden').addClass('new_product_shown');
-      $(this).removeClass("hidden_field").addClass("shown_field");
       $(this).html("Close")
     } else {
       $(".new_product_shown").removeClass('new_product_shown').addClass('new_product_hidden');
-      $(this).removeClass("shown_field").addClass("hidden_field");
       $(this).html("Add product")
     };
   });
