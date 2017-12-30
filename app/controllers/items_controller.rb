be5fbb84c9de4c9f0bdcdc9e3dbc3beafb5c3563
Andrew Levy <andrewglevy@gmail.com>
2017-12-29 18:56:26 -0800

    Added close button to editing items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :update, :destroy, :show]
   def create
     @item = Item.new(item_params)
 
@@ -15,39 +15,45 @@ class ItemsController < ApplicationController
     end
   end
 
-    def update
-      respond_to do |format|
-        if @item.update(item_params)
-          format.html { redirect_to @item.collection, notice: 'Item was successfully updated.' }
-          format.json { render :show, status: :ok, location: @collection }
-          format.js
-        else
-          format.html { render 'collections/show' }
-          format.json { render json: @collection.errors, status: :unprocessable_entity }
-          format.js
-        end
+  def show
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item.collection, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @collection }
+        format.js { render 'show' }
+      else
+        format.html { render 'collections/show' }
+        format.json { render json: @collection.errors, status: :unprocessable_entity }
+        format.js { render 'show' }
       end
     end
+  end
 
-    def destroy
-      @item.destroy
-      respond_to do |format|
-        format.html { redirect_to @item.collection, notice: 'Item was successfully destroyed.' }
-        format.json { head :no_content }
-        format.js
-      end
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to @item.collection, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js
     end
+  end
 
-    def edit
-      @collection = @item.collection
-    end
+  def edit
+    @collection = @item.collection
+  end
 
-  private
-    def set_item
-      @item = Item.find(params[:id])
-    end
+private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-    def item_params
-      params.require(:item).permit(:name, :status_id, :collection_id, :notes)
-    end
+  def item_params
+    params.require(:item).permit(:name, :status_id, :collection_id, :notes)
+  end
 end
